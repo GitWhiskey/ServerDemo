4d2f53313886ab8b8b473a5f6eaad47a4ba189ed
Jenya95 <sanevichj@gmail.com>
2017-02-28 11:51:19 +0300

    Добавил автоматическую таблицу. Bad works

--- src/main/resources/static/js/UserFinder.js
+++ src/main/resources/static/js/UserFinder.js
@@ -12,6 +12,66 @@ $(document).ready(function () {
         }
     });
 
+
+    //TODO Эта срань обрабатывает события только первый раз при загрузке страницы
+    $("#addName").on("keypress",(function(event) {
+        if (event.keyCode == 13) {
+            addUser();
+        }
+    }));
+
+    $("#addUserName").on("keypress",(function(event) {
+        if (event.keyCode == 13) {
+            addUser();
+        }
+    }));
+
+    $("#addBirthDay").on("keypress",(function(event) {
+        if (event.keyCode == 13) {
+            addUser();
+        }
+    }));
+
+
+    // updateBind();
+    //
+    // function updateBind() {
+    //
+    //     $("#addName").keypress(function (event) {
+    //         if (event.keyCode == 13) {
+    //             addUser();
+    //         }
+    //     });
+    //
+    //     $("#addUserName").keypress(function (event) {
+    //         if (event.keyCode == 13) {
+    //             addUser();
+    //         }
+    //     });
+    //
+    //     $("#addBirthDay").keypress(function (event) {
+    //         if (event.keyCode == 13) {
+    //             addUser();
+    //         }
+    //     });
+    // }
+
+
+    //Функция добавления нового пользователя
+    function addUser(){
+        var dogName = $("#addName").val();
+        var dogLogin = $("#addUserName").val();
+        var dogDate = $("#addBirthDay").val();
+
+        if ((dogName != "") && (dogLogin != "") && (dogDate != "")) {
+            $("#user-list").load("UserList/add?userName=" + dogName + "&userPass=" + dogLogin +
+                "&userDate="+ dogDate + "&userLogin=" + dogLogin);
+        }
+        else {
+            alert("Пустого пса нельзя добавить!");
+        }
+    }
+
     findingButton.click(function () {
         var username =  $("#search-input").val();
         $("#user-list").load("UserList/find?username=" + username);
