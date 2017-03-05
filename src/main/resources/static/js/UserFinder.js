1a1ac2dbd4a313a747b34c6b38d8e498d9c1529c
Jenya95 <sanevichj@gmail.com>
2017-03-05 19:46:49 +0300

    Merge branch 'master' of D:\projects\ServerDemo with conflicts.

--- src/main/resources/static/js/UserFinder.js
+++ src/main/resources/static/js/UserFinder.js
@@ -5,12 +5,89 @@ $(document).ready(function () {
 
     var findingButton = $("#find-users");
     var addButton = $("#addDog");
-    // Привязка кнопки enter к нажатию на кнопку
-    $("#search-input").keypress(function (event) {
+    var input = $("#search-input");
+    var action = true;
+
+    function recognize() {
+        window.ya.speechkit.settings.apikey = 'c4936434-79dd-4fd4-9edf-86c25699c967';
+        ya.speechkit.recognize({
+            // Функция будет вызвана, когда распознавание завершится.
+            doneCallback: function(text) {
+                if (text!="")
+                    input.val(text);
+                console.log("Финальный результат распознавания: " + text);
+            },
+            // Функция вызовется, как только сессия будет инициализирована.
+            initCallback: function () {
+                input.focus();
+                console.log("Процесс распознавания запущен.");
+            },
+            // Вызывается в случае возникновения ошибки.
+            errorCallback: function(err) {
+                console.log("Возникла ошибка: " + err);
+            },
+            // Длительность промежутка тишины, при наступлении которой
+            // распознавание завершается.
+            utteranceSilence: 5
+        });
+    }
+
+    // Привязка кнопки enter & ctrl к нажатию на кнопку
+    input.keypress(function (event) {
         if (event.keyCode == 13) {
             findingButton.click();
         }
     });
+
+    // Привязка кнопки ctrl к нажатию на кнопку
+    input.keydown(function(eventObject){
+        if ((eventObject.which == 17)&&(action === true)) {
+            action = false;
+            recognize();
+        }
+    });
+
+    input.keyup(function(eventObject){
+        if (eventObject.which == 17) {
+            action = true;
+        }
+    });
+    
+    $("#addName").on("keypress",(function(event) {
+        if (event.keyCode == 13) {
+            addUser();
+        }
+    }));
+
+    $("#addUserName").on("keypress",(function(event) {
+        if (event.keyCode == 13) {
+            addUser();
+        }
+    }));
+
+    $("#addBirthDay").on("keypress",(function(event) {
+        if (event.keyCode == 13) {
+            addUser();
+        }
+    }));
+
+    //Функция добавления нового пользователя
+    function addUser(){
+        var dogName = $("#addName").val();
+        var dogLogin = $("#addUserName").val();
+        var dogDate = $("#addBirthDay").val();
+
+        if ((dogName != "") && (dogLogin != "") && (dogDate != "")) {
+            $("#user-list").load("UserList/add?userName=" + dogName + "&userPass=" + dogLogin +
+                "&userDate="+ dogDate + "&userLogin=" + dogLogin);
+            $("#addName").val("");
+            $("#addUserName").val("");
+            $("#addBirthDay").val("");
+        }
+        else {
+            alert("Пустого пса нельзя добавить!");
+        }
+    }
 
     findingButton.click(function () {
         var username =  $("#search-input").val();
